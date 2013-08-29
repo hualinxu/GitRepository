7fe74d27ccb0ed6c6d27d5fc113fa697e281ed7b
hualinxu <lin_snowing@163.com>
2013-08-29 14:20:58 +0800

    Signed-off-by: hualinxu <lin_snowing@163.com>

---
+++ test.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2012 Sohu. All Rights Reserved
+ */
+/**
+ * <p>
+ * Description:
+ * </p>
+ * @author hualinxu
+ * @version 1.0
+ * @Date 2013-8-29
+ */
+public class test {
+
+    public static void main(String[] args){
+        System.out.println("Helloworld");
+    }
+}
+
